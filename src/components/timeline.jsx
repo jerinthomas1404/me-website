fde4318d5ce69625ec011c623435447b1a2a244b
Jerin Thomas <jerinthomas1404@gmail.com>
2019-08-18 11:52:07 +0530

    Initial commit

---
+++ src/components/timeline.jsx
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div
+                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Internship at TCS <span>JUN-AUG,2019</span>
+                        </h2>
+                        <p>
+                          I recently have joined the TCS as a Software
+                          Developer-Intern in the CMA team. My major part of the
+                          work has been into the field of creating a webapp for
+                          API monitoring ans testing. I did also exploring the
+                          role of different cloud-services.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInTop"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Undergraduation at RAIT<span>2016(pursuing)</span>
+                        </h2>
+                        <p>
+                          I am pursuing my under-graduation in the field of
+                          Computer Science. I have taken courses like DSA, OOPs,
+                          Big Data over the years and have better understanding
+                          of these subjects.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Primary and Higher Education <span>2014-2016</span>
+                        </h2>
+                        <p>
+                          I have completed my high school education from Delhi
+                          Public School. During my time at school, I have
+                          developed interest in Mathematics and Computer
+                          Science.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry begin animate-box"
+                    data-animate-effect="fadeInBottom"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none" />
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
